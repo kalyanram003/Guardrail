2f775c669d0eb009f62ffbeae45c11964e813eb5
kalyanram003 <kalyanram2053@gmail.com>
2026-04-22 22:14:08 +0530

    first commit

---
+++ src/main/java/com/grid07/guardrail/GuardrailApplication.java
@@ -0,0 +1,13 @@
+package com.grid07.guardrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuardrailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuardrailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grid07/guardrail/GuardrailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grid07.guardrail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuardrailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
